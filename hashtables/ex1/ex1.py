57fe8635c6a29e0d4533c177c3abb699ef4b6e37
Stephen Tanksley <stephen.tanksley@gmail.com>
2020-08-15 12:10:26 -0500

    Mostly done but hit a bit of a roadblock. Moving on to the next one for now.

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -6,6 +6,8 @@
         Sub-Problem: What do we store in the table? 
         
         Sub-Solution: I think it would make sense to store the weight itself as the index. If we store the weight itself as the index, we can take the limit minus the weight of the first index to find the second index, then return both indexes as a tuple. When ordering the tuple, the larger weight needs to be in [0] position, the lighter index in [1] position.
+        
+        Because the function needs to run in linear time, I get to use a single for loop to solve this problem. No nested loops.
 
 """
 weights_3 = [4, 6, 10, 15, 16]
@@ -15,8 +17,14 @@ def get_indices_of_item_weights(weights, length, limit):
 
     table = {value: key for (key, value) in enumerate(weights)}
 
-    print(table)
+    for i in weights:
+
+        if (limit - i in table) and i in table:
+            pre_format = (table[limit - i], table[i])
+            formatted = tuple(sorted(pre_format, reverse=True))
+            return formatted
+
     return None
 
 
-get_indices_of_item_weights(weights_3, 5, 21)
+print(get_indices_of_item_weights(weights_3, 5, 21))
