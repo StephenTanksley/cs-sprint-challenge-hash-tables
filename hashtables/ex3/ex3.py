63585ea43fb7eef7c477faf794b7689fb73bd3e8
Stephen Tanksley <stephen.tanksley@gmail.com>
2020-08-17 11:59:10 -0500

    Checked the test.
    Exact same conditions fail in the test, but pass when manually checking. I'm missing something.

--- hashtables/ex3/ex3.py
+++ hashtables/ex3/ex3.py
@@ -33,7 +33,6 @@ def intersection(arrays):
         if count == len(arrays):
             result.append(number)
 
-    print(result)
     return result
 
 
@@ -45,3 +44,20 @@ if __name__ == "__main__":
     arrays.append(list(range(3000000, 4000000)) + [1, 2, 3])
 
     print(intersection(arrays))
+
+    # Function works on this side, but fails in the test. Not sure why?
+
+    # arrays = [
+    #         list(range(1000000, 2000000)) + [1, 2, 3],
+    #         list(range(2000000, 3000000)) + [1, 2, 3],
+    #         list(range(3000000, 4000000)) + [1, 2, 3],
+    #         list(range(4000000, 5000000)) + [1, 2, 3],
+    #         list(range(5000000, 6000000)) + [1, 2, 3],
+    #         list(range(6000000, 7000000)) + [1, 2, 3],
+    #         list(range(7000000, 8000000)) + [1, 2, 3],
+    #         list(range(8000000, 9000000)) + [1, 2, 3],
+    #         list(range(9000000, 10000000)) + [1, 2, 3],
+    #         list(range(10000000, 11000000)) + [1, 2, 3]
+    #     ]
+
+    # print(intersection(arrays))
