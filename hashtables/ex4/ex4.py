3a5b98c18d681fddb6861014e71337f3cb0680cb
Stephen Tanksley <stephen.tanksley@gmail.com>
2020-08-17 19:42:39 -0500

    Fixed some faulty logic.

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -18,33 +18,21 @@ def has_negatives(a):
 
     # Create a list for the result.
     result = []
+    negatives = []
 
     # For every item, we want to populate a table with that item in it.
     for item in a:
-        absolute_value = abs(item)
-        if absolute_value not in table:
-            table[absolute_value] = 1
-
-        # If the item is already in the table, we can update our count for it.
-        else:
-            table[absolute_value] += 1
-
-    # We want to check and see if there's more than one of these values in there. The lists are going to include some items which are in there multiple times. Those will be reflected in the count. So if the count is more than 1, we return that item to the result list.
-    for number, count in table.items():
-        if count > 1:
-            result.append(number)
-
-    return result
-
-
-"""
-    for item in a:
         if item > 0:
             table[item] = 1
-        
-        if item < 0 and abs(item) in table:
-            table[abs(item)] += 1
-"""
+        else:
+            negatives.append(item)
+
+    for item in negatives:
+        absolute_value = abs(item)
+        if absolute_value in table:
+            result.append(absolute_value)
+
+    return result
 
 
 if __name__ == "__main__":
